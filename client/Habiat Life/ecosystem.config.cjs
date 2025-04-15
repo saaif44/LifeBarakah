2ec3bde457348432f8067bda0a53918082b9bce8
saaif44 <saaifshuvo@gmail.com>
2025-04-15 12:55:59 +0600

    Local update before pull

---
+++ client/Habiat Life/ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: 'lifebarakah-frontend',
+      script: './start-frontend.sh',
+      cwd: "/home/saaif44/projects/LifeBarakah/client/Habiat Life",
+    },
+  ],
+};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+//promethues support to check server status
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -6,12 +11,21 @@ const habitRoutes = require('./routes/habitRoutes');
 const pool = require('./config/db');
 // const protect = require('./middleware/authMiddleware');
 
+
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/habits', habitRoutes);
 
